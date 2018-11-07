ea92279165e3850ecead5b1c5c0851137cc0f891
JonPoret <44809358+JonPoret@users.noreply.github.com>
2018-11-07 08:14:02 -0500

    Update ChatBot.java

--- CompSciChatBot/src/ChatBot.java
+++ CompSciChatBot/src/ChatBot.java
@@ -1,7 +1,7 @@
 
 public class ChatBot {
   public static void main(String[] args) {
-    String keywords[] = {"processor", "ram", "cooler", "graphics card", "case", "motherboard", "power supply", "i/o", "ports", "peripherals", "operating system", "asics", "risers", "daisy chaining", "sli", "crossfire", "resolution", "refresh rate"}
+    String keywords[] = {"processor", "ram", "cooler", "graphics card", "case", "motherboard", "power supply", "i/o", "ports", "peripherals", "operating system", "asics", "risers", "daisy chaining", "sli", "crossfire", "resolution", "refresh rate", "response time", "core count", "frequence", "fans", "intel", "amd", "nvidia", "atx", "mid tower", "itx", "windows", "macos", "linux", "ubuntu", "kali", "rasbian", "arduino", "raspberry pi"}
     SOP("Hello, Welcome to my Computer Hardware bot! \nThanks for using me!\nPlease type yes to use this bot, or no to exit: ");
     Scanner scan = new scanner(System.in);
     String useQuery = scan.next();
