55108866aa6c1e5d94fac25367f0afaac870c931
JonPoret <44809358+JonPoret@users.noreply.github.com>
2018-11-12 07:46:46 -0500

    Update ChatBot.java
    fixed errors - im a big dumb

--- CompSciChatBot/src/ChatBot.java
+++ CompSciChatBot/src/ChatBot.java
@@ -1,20 +1,23 @@
+// Author: Jon Poret
+// Date: 11/6/2018
+// Purpose: To create a chatbot
 
+import java.util.Scanner;
 public class ChatBot {
   public static void main(String[] args) {
     String keywords[] = {"processor", "ram", "cooler", "graphics card", "case", "motherboard", "power supply", "i/o", "ports", "peripherals", "operating system", "asics", "risers", "daisy chaining", "sli", "crossfire", "resolution", "refresh rate", "response time", "core count", "frequence", "fans", "intel", "amd", "nvidia", "atx", "mid tower", "itx", "windows", "macos", "linux", "ubuntu", "kali", "rasbian", "arduino", "raspberry pi"};
     SOP("Hello, Welcome to my Computer Hardware bot! \nThanks for using me!\nPlease type yes to use this bot, or no to exit: ");
-    Scanner scan = new scanner(System.in);
+    Scanner scan = new Scanner(System.in);
     String useQuery = scan.next();
-    if (useQuery.toLowerCase().equals("no") {
+    if (useQuery.toLowerCase().equals("no")) {
       SOP("Goodbye!");
-      break;
     }
-    else if (useQuery.toLowercase().equals("yes") {
+    else if (useQuery.toLowerCase().equals("yes")) {
       while(true) {
         SOP("Please enter the computer term you would like to learn more about: ");
-        Scanner scaner1 = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         String termasked = scanner1.next();
-    }
+    }}
   }
 
   public static void SOP(String s) {
