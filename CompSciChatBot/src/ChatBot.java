8e5c035cc8f223296c9e67aea1a5cf1540973df0
JonPoret <44809358+JonPoret@users.noreply.github.com>
2018-11-12 07:49:36 -0500

    Update ChatBot.java
    Added a "pront" function - it's the same as SOP (System.out.println) but instead of using println it just does print so allow scanners to look cleaner

--- CompSciChatBot/src/ChatBot.java
+++ CompSciChatBot/src/ChatBot.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ChatBot {
   public static void main(String[] args) {
     String keywords[] = {"processor", "ram", "cooler", "graphics card", "case", "motherboard", "power supply", "i/o", "ports", "peripherals", "operating system", "asics", "risers", "daisy chaining", "sli", "crossfire", "resolution", "refresh rate", "response time", "core count", "frequence", "fans", "intel", "amd", "nvidia", "atx", "mid tower", "itx", "windows", "macos", "linux", "ubuntu", "kali", "rasbian", "arduino", "raspberry pi"};
-    SOP("Hello, Welcome to my Computer Hardware bot! \nThanks for using me!\nPlease type yes to use this bot, or no to exit: ");
+    pront("Hello, Welcome to my Computer Hardware bot! \nThanks for using me!\nPlease type yes to use this bot, or no to exit: ");
     Scanner scan = new Scanner(System.in);
     String useQuery = scan.next();
     if (useQuery.toLowerCase().equals("no")) {
@@ -14,7 +14,7 @@ public class ChatBot {
     }
     else if (useQuery.toLowerCase().equals("yes")) {
       while(true) {
-        SOP("Please enter the computer term you would like to learn more about: ");
+        pront("Please enter the computer term you would like to learn more about: ");
         Scanner scanner1 = new Scanner(System.in);
         String termasked = scanner1.next();
     }}
@@ -22,5 +22,9 @@ public class ChatBot {
 
   public static void SOP(String s) {
     System.out.println(s);
+  }
+  
+  public static void pront(String c) {
+	  System.out.print(c);
   }
 }
